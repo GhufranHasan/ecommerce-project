7404811adaf2702fa9c6677bbef8acace78f8ea5
Ghufran Hasan <ghufran.hasan@o3interfaces.com>
2023-03-13 17:33:07 +0500

    Routing (Two links) added - [Home] & [Seller Auth]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'seller-auth',
+    component: SellerAuthComponent
+  },
+  {
+    path: 'login',
+    // component: 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
